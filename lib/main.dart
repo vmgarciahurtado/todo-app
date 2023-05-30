ebbdfe6d34dd8042d8e8c28d9d528928cc2b7247
vamgarcia <victor.garcia@celuweb.com>
2023-05-30 00:35:21 -0500

    configure firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:todo_app/firebase_options.dart';
 
-void main() {
+void main() async {
   runApp(const MyApp());
+}
+
+Future<void> initFirebase() async {
+  try {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  } catch (e) {
+    return;
+  }
 }
 
 class MyApp extends StatelessWidget {
