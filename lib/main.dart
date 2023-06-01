fa5c1c631a5db52b245fcd16cbbc4df63b4565c6
vamgarcia <victor.garcia@celuweb.com>
2023-06-01 00:43:54 -0500

    remove firebase libraries

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,7 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:todo_app/firebase_options.dart';
 
 void main() async {
   runApp(const MyApp());
-}
-
-Future<void> initFirebase() async {
-  try {
-    await Firebase.initializeApp(
-      options: DefaultFirebaseOptions.currentPlatform,
-    );
-  } catch (e) {
-    return;
-  }
 }
 
 class MyApp extends StatelessWidget {
